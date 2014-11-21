1f734b6fb490db4e414894959a8d2c71f676ed49
toka-htwg <tobias.spott@htwg-konstanz.de>
2014-11-21 19:50:42 +0100

    211114: Initialer Commit in das neue Repo / Projekt an Play angepasst

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,11 +8,59 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-         return ok(index.render("Your new application is ready."));
+         return ok(index.render());
     }
 
-	// 17112014: mal testen
-	public static Result login() {
-		return ok("Test");
+	 public static Result contact() {
+		return ok(contact.render());
 	}
+	
+	public static Result goodbye() {
+		return ok(goodbye.render());
+	}
+	
+	public static Result agbs() {
+		return ok(agbs.render());
+	}
+	
+	public static Result impressum() {
+		return ok(impressum.render());
+	}
+	
+	public static Result information_1hotel() {
+		return ok(information_1hotel.render());
+	}
+	
+	public static Result login() {
+		return ok(login.render());
+	}
+	
+	public static Result newsletter() {
+		return ok(newsletter.render());
+	}
+	
+	public static Result profile() {
+		return ok(profile.render());
+	}
+	
+	public static Result rate_hotel() {
+		return ok(rate_hotel.render());
+	}
+	
+	public static Result registration() {
+		return ok(registration.render());
+	}
+	
+	public static Result results_search_hotel() {
+		return ok(results_search_hotel.render());
+	}
+	
+	public static Result search_hotel() {
+		return ok(search_hotel.render());
+	}
+	
+	public static Result welcome() {
+		return ok(welcome.render());
+	}
+	
 }
